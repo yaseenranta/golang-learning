4c336336e905de1a159142d82f0c45f0dfbd16d8
Muhammad Yaseen <yaseenranta76@gmail.com>
2021-04-14 15:06:01 +0500

    cancel

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,29 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
--- main.go
+++ main.go
@@ -8,6 +8,6 @@ import (
 
 func main() {
 	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	message := greetings.Hello("Go")
 	fmt.Println(message)
 }
