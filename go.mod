a9bb1b74d7b272f023e5c0304f445428443b7de3
Muhammad Yaseen <yaseenranta76@gmail.com>
2021-11-27 19:06:40 +0500

    json.Marshal practice.

--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module github.com/yaseenranta/golang-learning
 
 go 1.16
-
-require example.com/greetings v0.0.0-00010101000000-000000000000
-
-replace example.com/greetings => ./greetings
--- main.go
+++ main.go
@@ -1,13 +1,68 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-
-	"example.com/greetings"
+	"time"
 )
 
+/**
+JSON Package Notes
+	json:"-" remove field from json
+	omitempty remove field when its empty
+	 MarshalJSON implement formating field
+**/
+
+type Address struct {
+	Addressline1 string `json:"address_line1"`
+	AddressLine2 string `json:"address_line2"`
+	City         string `json:"city"`
+	State        string `json:"state"`
+	ZipCode      string `json:"zipcode"`
+	Country      string `json:"country,omitempty"`
+}
+
+type Person struct {
+	Name       string     `json:"name"`
+	Age        int        `json:"-"`
+	Profession string     `json:"profession"`
+	Address    Address    `json:"address"`
+	CreatedAt  FormatDate `json:"created_at"`
+	DeletedAt  FormatDate `json:"deleted_at,omitempty"`
+}
+
+const dateFormat = "2006-01-02"
+
+type FormatDate struct {
+	time.Time
+}
+
+func (f FormatDate) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("\"%s\"", f.Format(dateFormat))), nil
+}
+
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
-	fmt.Println(message)
+	t := time.Now()
+	p := Person{
+		Name:       "Muhammad Yaseen",
+		Age:        29,
+		Profession: "Developer",
+		Address: Address{
+			Addressline1: "house # 1111",
+			AddressLine2: "area 311/b",
+			City:         "karachi",
+			State:        "sindh",
+			ZipCode:      "33223",
+			// Country:      "PK",
+		},
+		CreatedAt: FormatDate{t},
+	}
+
+	person, err := json.Marshal(p)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(person))
 }
